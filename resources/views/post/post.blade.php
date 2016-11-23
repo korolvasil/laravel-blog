edbf2157cc2a31954e87701a9fb8469a2487057a
vagrant <kingvasil@gmail.com>
2016-11-23 00:22:54 +0000

    The Post body

--- resources/views/post/post.blade.php
+++ resources/views/post/post.blade.php
@@ -1,5 +1,56 @@
 @extends('templates.app')
 
 @section('main')
-    Article Page
+    <article class="article">
+        <h1 class="article__header">Lorem ipsum dolor sit amet.</h1>
+        <h2 class="article__subheader">Lorem ipsum dolor sit amet, consectetur adipisicing.</h2>
+        <div class="article__body">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Accusamus ad aliquid aspernatur atque consequatur cum delectus dolores exercitationem facere facilis
+                id illum incidunt labore laborum magni minima molestias non odit praesentium quibusdam ratione,
+                saepe similique sit tenetur totam vel vero.
+                Atque consequatur doloremque doloribus expedita illum optio pariatur quisquam sequi.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Aut blanditiis commodi deserunt et quam, sed veniam? Adipisci aliquam amet autem consequatur
+                dolore ducimus earum, eveniet illo inventore itaque iusto, maxime mollitia nam nesciunt nobis officia
+                optio perspiciatis quia quidem sit tempore ullam velit vero.
+                A accusamus architecto assumenda, aut consequuntur error eveniet illum nobis obcaecati officiis quis
+                unde veniam voluptatum.
+                Ad eligendi enim inventore maxime nostrum officiis quis repudiandae veniam.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Ad aliquid, consectetur dolore dolores dolorum eos exercitationem facilis harum
+                laudantium libero magni minus molestiae molestias mollitia non odit officiis
+                possimus quas quis quo sequi sunt ullam.
+                Doloribus minima natus tempora vero.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Accusamus ad aliquid aspernatur atque consequatur cum delectus dolores exercitationem facere facilis
+                id illum incidunt labore laborum magni minima molestias non odit praesentium quibusdam ratione,
+                saepe similique sit tenetur totam vel vero.
+                Atque consequatur doloremque doloribus expedita illum optio pariatur quisquam sequi.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Aut blanditiis commodi deserunt et quam, sed veniam? Adipisci aliquam amet autem consequatur
+                dolore ducimus earum, eveniet illo inventore itaque iusto, maxime mollitia nam nesciunt nobis officia
+                optio perspiciatis quia quidem sit tempore ullam velit vero.
+                A accusamus architecto assumenda, aut consequuntur error eveniet illum nobis obcaecati officiis quis
+                unde veniam voluptatum.
+                Ad eligendi enim inventore maxime nostrum officiis quis repudiandae veniam.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Ad aliquid, consectetur dolore dolores dolorum eos exercitationem facilis harum
+                laudantium libero magni minus molestiae molestias mollitia non odit officiis
+                possimus quas quis quo sequi sunt ullam.
+                Doloribus minima natus tempora vero.
+            </p>
+        </div>
+    </article>
 @endsection
